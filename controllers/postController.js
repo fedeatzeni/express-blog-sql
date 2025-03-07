cf32e8237cfe3a69c28cbd472c2b5436c27db447
Fede <fede.atzeni372@gmail.com>
2025-03-07 17:07:15 +0100

    feat show tags

--- controllers/postController.js
+++ controllers/postController.js
@@ -25,7 +25,24 @@ function show(req, res) {
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
         if (results.length === 0) return res.status(404).json({ error: 'Pizza not found' });
-        res.json(results[0]);
+        // res.json(results[0]);
+
+        const post = results[0];
+
+        //tags query
+        const tagSql = `
+            SELECT tags.*
+            FROM tags
+            JOIN post_tag ON post_tag.tag_id = tags.id
+            WHERE post_tag.post_id = ?
+        `
+        connection.query(tagSql, [id], (err, resultsTags) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+            post.tags = resultsTags
+
+            res.json(post);
+        });
+
     });
 };
 
